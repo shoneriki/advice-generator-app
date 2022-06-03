ef973d195bd56a597dc586d26cd4b9ef185575d1
shoneriki <ikirenohs@gmail.com>
2022-06-03 20:09:19 +0900

    break added

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+const btn =  document.getElementById("dataBtn")
 const result = document.getElementById("advice");
 const number = document.getElementById("advice-number")
-const btn=  document.getElementById("dataBtn")
+
+const url = "https://api.adviceslip.com/advice";
+
+// btn.addEventListener('click', async () => {
+//   const response = await fetch(url);
+//   const json = await response.json();
+//   const {advice, id} = json.slip;
+//   number.textContent = id;
+//   result.textContent = advice;
+// })
 
 fetch("https://api.adviceslip.com/advice")
   .then(response => {
     return response.json();
   }).then(adviceData => {
-    console.log(adviceData);
     result.innerText = `"${adviceData.slip.advice}"`;
     number.innerText = `#${adviceData.slip.id}`
   }).catch(error => {
